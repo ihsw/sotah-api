ffc89aa3484258bb02aa94f69aff18937e0d7a12
Adrian Parker <ihsw.aparker@gmail.com>
2017-08-19 12:30:31 -0400

    Remove extra error handling, instead opting for general error handler.

--- app/src/lib/app.ts
+++ app/src/lib/app.ts
@@ -3,36 +3,26 @@ import { wrap } from "async-middleware";
 import * as HttpStatus from "http-status";
 
 import { default as Messenger } from "./messenger";
-import { IRegion } from "./region";
 
 export default (messenger: Messenger): express.Express => {
   const app = express();
 
   app.get("/", (_, res) => res.send("Hello, world!"));
   app.get("/regions", wrap(async (_, res) => {
-    let regions: IRegion[];
-    try {
-      regions = await messenger.getRegions();
-    } catch (err) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err.message).end();
-
-      return;
-    }
-
+    const regions = await messenger.getRegions();
     res.send(regions).end();
   }));
   app.get("/status/:regionName", wrap(async (req, res) => {
-    let status: string = "";
-    try {
-      status = await messenger.getStatus(req.params["regionName"]);
-    } catch (err) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err.message).end();
-
-      return;
-    }
-
+    const status = await messenger.getStatus(req.params["regionName"]);
     res.send(status).end();
   }));
+  app.get("/internal-error", () => {
+    throw new Error("Test error!");
+  })
+  app.use((err: Error, _: express.Request, res: express.Response, next: Function) => {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err.message);
+    next();
+  });
 
   return app;
 };
