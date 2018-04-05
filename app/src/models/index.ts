04526ca031684aec0a79577a1e2fdb1a305ba702
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-04-05 12:19:37 -0400

    Add missing call on appending user relationships to user model.

--- app/src/models/index.ts
+++ app/src/models/index.ts
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 import {
   UserModel,
-  createModel as createUserModel
+  createModel as createUserModel,
+  appendRelationships as appendUserRelationships
 } from "./user";
 
 type Models = {
@@ -11,6 +12,7 @@ type Models = {
 
 export const createModels = (sequelize: Sequelize): Models => {
   let User = createUserModel(sequelize);
+  User = appendUserRelationships(User);
 
   return { User };
 };
