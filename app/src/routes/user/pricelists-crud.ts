4f528e94715febf65df52c7e02f103d4a55221a3
Adrian Parker <ihsw.aparker@gmail.com>
2018-07-20 08:50:16 -0400

    Remove unnecessary console log.

--- app/src/routes/user/pricelists-crud.ts
+++ app/src/routes/user/pricelists-crud.ts
@@ -85,8 +85,6 @@ export const getRouter = (models: Models) => {
       return;
     }
 
-    console.log(result);
-
     pricelist.setAttributes({ ...result.pricelist });
     pricelist.save();
     res.json({ pricelist: pricelist.toJSON() });
