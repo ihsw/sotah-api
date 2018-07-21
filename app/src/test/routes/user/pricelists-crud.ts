363c01f9a8d608306d90327a3dfea15428c73a9a
Adrian Parker <ihsw.aparker@gmail.com>
2018-07-20 20:28:05 -0400

    Add a route for validating no pricelists for an invalid region/realm.

--- app/src/test/routes/user/pricelists-crud.ts
+++ app/src/test/routes/user/pricelists-crud.ts
@@ -121,6 +121,33 @@ test("Pricelists crud endpoint Should return pricelists for a region/realm", asy
   );
 });
 
+test("Pricelists crud endpoint Should return no pricelists for an invalid region/realm", async (t) => {
+  const password = "test";
+  const user = await createUser(t, {
+    email: `get-pricelists+${uuidv4()}@test.com`,
+    password
+  });
+  let res = await request.post("/login").send({ email: user.email, password });
+  t.is(res.status, HTTPStatus.OK);
+  const { token } = res.body;
+
+  const count = 5;
+  for (let i = 0; i < count; i++) {
+    await createPricelist(t, res.body.token, {
+      entries: [{item_id: -1, quantity_modifier: -1}],
+      pricelist: { name: "test", realm: "test-realm", region: "test-region" }
+    });
+  }
+
+  res = await (request
+    .get("/user/pricelists/region/wew/realm/lad")
+    .set("Authorization", `Bearer ${token}`)
+  );
+  const { body, status } = res;
+  t.is(status, HTTPStatus.OK);
+  t.is(body.pricelists.length, 0);
+});
+
 test("Pricelists crud endpoint Should update a pricelist", async (t) => {
   const password = "test";
   const user = await createUser(t, {
