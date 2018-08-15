84c849408183d96e87818da499a91d2bd796ac07
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-15 17:49:01 -0400

    Add tests for profession-pricelists route.

--- sotah-api/app/src/test/routes/user/profession-pricelists-crud.ts
+++ sotah-api/app/src/test/routes/user/profession-pricelists-crud.ts
@@ -13,9 +13,9 @@ const { request } = setup({
   natsHost: process.env["NATS_HOST"] as string,
   natsPort: process.env["NATS_PORT"] as string
 });
-const { createUser, requestProfessionPricelist } = getTestHelper(request);
+const { createUser, requestProfessionPricelist, createProfessionPricelist } = getTestHelper(request);
 
-test.only("Profession pricelists crud endpoint Should create a profession-pricelist", async (t) => {
+test("Profession pricelists crud endpoint Should create a profession-pricelist", async (t) => {
   const password = "testtest";
   const user = await createUser(t, {
     email: `create-profession-pricelist+${uuidv4()}@test.com`,
@@ -41,4 +41,86 @@ test.only("Profession pricelists crud endpoint Should create a profession-pricel
 
   t.true("entries" in body);
   t.is(body.entries.length, 1);
+});
+
+test("Profession pricelists crud endpoint Should return profession-pricelists", async (t) => {
+  const password = "testtest";
+  const user = await createUser(t, {
+    email: `get-profession-pricelists+${uuidv4()}@test.com`,
+    password
+  });
+  let res = await request.post("/login").send({ email: user.email, password });
+  t.is(res.status, HTTPStatus.OK);
+  const { token } = res.body;
+
+  const count = 5;
+  for (let i = 0; i < count; i++) {
+    await createProfessionPricelist(t, token, {
+      entries: [{item_id: -1, quantity_modifier: -1}],
+      pricelist: { name: "test", realm: "test-realm", region: "test-region" },
+      profession_name: "jewelcrafting"
+    });
+  }
+
+  res = await (request
+    .get("/user/profession-pricelists/region/test-region/realm/test-realm")
+    .set("Authorization", `Bearer ${token}`)
+  );
+  const { body, status } = res;
+  t.is(status, HTTPStatus.OK);
+  t.is(body.profession_pricelists.length, 5);
+  t.is(
+    body.profession_pricelists.reduce((total, v) => total + v.pricelist.pricelist_entries.length, 0),
+    5
+  );
+});
+
+test("Profession pricelists crud endpoint Should delete a profession-pricelist", async (t) => {
+  const password = "testtest";
+  const user = await createUser(t, {
+    email: `delete-pricelist+${uuidv4()}@test.com`,
+    password
+  });
+  let res = await request.post("/login").send({ email: user.email, password });
+  t.is(res.status, HTTPStatus.OK);
+  const { token } = res.body;
+
+  const responseBody = await createProfessionPricelist(t, token, {
+    entries: [{item_id: -1, quantity_modifier: -1}],
+    pricelist: { name: "test", realm: "test-realm", region: "test-region" },
+    profession_name: "jewelcrafting"
+  });
+
+  res = await request.delete(`/user/profession-pricelists/${responseBody.profession_pricelist.id}`)
+    .set("Authorization", `Bearer ${token}`);
+  t.is(res.status, HTTPStatus.OK);
+});
+
+test("Profession pricelists crud endpoint Should fail on deleting a non-owned profession-pricelist", async (t) => {
+  const password = "testtest";
+  const user = await createUser(t, {
+    email: `delete-other-pricelist+${uuidv4()}@test.com`,
+    password
+  });
+  let res = await request.post("/login").send({ email: user.email, password });
+  t.is(res.status, HTTPStatus.OK);
+  const { token } = res.body;
+
+  const responseBody = await createProfessionPricelist(t, token, {
+    entries: [{item_id: -1, quantity_modifier: -1}],
+    pricelist: { name: "test", realm: "test-realm", region: "test-region" },
+    profession_name: "jewelcrafting"
+  });
+
+  const otherUser = await createUser(t, {
+    email: `delete-other-pricelist+${uuidv4()}@test.com`,
+    password
+  });
+  res = await request.post("/login").send({ email: otherUser.email, password });
+  t.is(res.status, HTTPStatus.OK);
+  const { token: otherToken } = res.body;
+
+  res = await request.delete(`/user/profession-pricelists/${responseBody.profession_pricelist.id}`)
+    .set("Authorization", `Bearer ${otherToken}`);
+  t.is(res.status, HTTPStatus.UNAUTHORIZED);
 });
