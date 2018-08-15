93e64d347fe3100bfd94954384372807ef394174
Adrian Parker <ihsw.aparker@gmail.com>
2018-08-15 16:22:41 -0400

    Add validator rules for profession-pricelist.

--- sotah-api/app/src/lib/validator-rules.ts
+++ sotah-api/app/src/lib/validator-rules.ts
@@ -22,6 +22,12 @@ export const PricelistRequestBodyRules = yup.object().shape({
   pricelist: PricelistRules.required()
 }).noUnknown();
 
+export const ProfessionPricelistRequestBodyRules = yup.object().shape({
+  entries: yup.array(PriceListEntryRules).required(),
+  pricelist: PricelistRules.required(),
+  profession_name: yup.string().required()
+}).noUnknown();
+
 export const UserRequestBodyRules = yup.object().shape({
   email: yup.string().email("Email must be a valid email").required("Email is required"),
   password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required")
