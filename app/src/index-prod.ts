359753df3c59404b91a5745ce7ae14fa065599a4
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-01 17:55:53 -0400

    Create prod run script.

---
+++ app/src/index-prod.ts
@@ -0,0 +1,32 @@
+import * as cluster from "cluster";
+import * as process from "process";
+import * as http from "http";
+import * as os from "os";
+
+import { getApp } from "./lib/app";
+import { getLogger } from "./lib/logger";
+
+// env-var loading
+const natsHost = process.env["NATS_HOST"] || "";
+const natsPort = process.env["NATS_PORT"] || "";
+const dbHost = process.env["DB_HOST"] || "";
+const appPort = process.env["APP_PORT"];
+
+// logger init
+const logger = getLogger("debug");
+
+if (cluster.isMaster) {
+  const numCpus = os.cpus().length;
+
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    logger.info("Worker exited", { code, pid: worker.process.pid, signal });
+  });
+} else {
+  const app = getApp({ logger, natsHost, natsPort, dbHost });
+  const server = http.createServer(app);
+  server.listen(appPort, () => logger.info("Listening", { port: appPort }));
+}
