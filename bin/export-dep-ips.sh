acb0693dbb515537e8139b821d01dedcc7837861
Adrian Parker <ihsw.aparker@gmail.com>
2018-09-02 09:00:43 -0400

    Add script for exporting appropriate env vars.

---
+++ bin/export-dep-ips.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+export DB_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_db_1`
+export NATS_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_nats_1`
+export NATS_PORT=4222
+export APP_PORT=8080
