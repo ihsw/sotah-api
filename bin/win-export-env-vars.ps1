b65d8d070cbb687ab88ca981a018edbbc63b195e
Adrian Parker <ihsw.aparker@gmail.com>
2018-10-06 14:43:06 -0400

    Fix env vars.

--- bin/win-export-env-vars.ps1
+++ bin/win-export-env-vars.ps1
@@ -1,5 +1,5 @@
-$Env:DB_HOST = docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotah-infra_db-dev_1
-$Env:NATS_HOST = docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotah-infra_nats_1
+$Env:DB_HOST = 'localhost'
+$Env:NATS_HOST = 'localhost'
 $Env:NATS_PORT = 4222
 $Env:APP_PORT = 8080
 Write-Output "wew lad"
