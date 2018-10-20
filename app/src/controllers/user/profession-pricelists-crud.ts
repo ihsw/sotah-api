0ab0a5434c24be5aff5b3f1203e942bbea54caaf
Adrian Parker <ihsw.aparker@gmail.com>
2018-10-20 18:17:28 -0400

    Create profession-pricelists-crud controller.

---
+++ app/src/controllers/user/profession-pricelists-crud.ts
@@ -0,0 +1,118 @@
+import * as HTTPStatus from "http-status";
+import { Connection } from "typeorm";
+
+import { Pricelist, PricelistEntry, ProfessionPricelist } from "../../entities";
+import { UserLevel } from "../../entities/user";
+import { ProfessionPricelistRequestBodyRules } from "../../lib/validator-rules";
+import { IValidationErrorResponse } from "../contracts";
+import {
+    ICreateProfessionPricelistRequest,
+    ICreateProfessionPricelistResponse,
+} from "../contracts/user/profession-pricelists-crud";
+import { RequestHandler } from "../index";
+
+export class ProfessionPricelistsCrudController {
+    private dbConn: Connection;
+
+    constructor(dbConn: Connection) {
+        this.dbConn = dbConn;
+    }
+
+    public createProfessionPricelist: RequestHandler<
+        ICreateProfessionPricelistRequest,
+        ICreateProfessionPricelistResponse | IValidationErrorResponse
+    > = async req => {
+        const user = req.user!;
+        if (user.level !== UserLevel.Admin) {
+            const validationErrors: IValidationErrorResponse = { unauthorized: "You are not authorized to do that." };
+
+            return {
+                data: validationErrors,
+                status: HTTPStatus.UNAUTHORIZED,
+            };
+        }
+
+        let result: ICreateProfessionPricelistRequest | null = null;
+        try {
+            result = (await ProfessionPricelistRequestBodyRules.validate(
+                req.body,
+            )) as ICreateProfessionPricelistRequest;
+        } catch (err) {
+            const validationErrors: IValidationErrorResponse = { [err.path]: err.message };
+
+            return {
+                data: validationErrors,
+                status: HTTPStatus.BAD_REQUEST,
+            };
+        }
+
+        const pricelist = new Pricelist();
+        pricelist.user = user;
+        pricelist.name = result.pricelist.name;
+        await this.dbConn.manager.save(pricelist);
+
+        const entries = await Promise.all(
+            result.entries.map(v => {
+                const entry = new PricelistEntry();
+                entry.pricelist = pricelist;
+                entry.itemId = v.item_id;
+                entry.quantityModifier = v.quantity_modifier;
+
+                return this.dbConn.manager.save(entry);
+            }),
+        );
+
+        const professionPricelist = new ProfessionPricelist();
+        professionPricelist.pricelist = pricelist;
+        professionPricelist.name = result.profession_name;
+        professionPricelist.expansion = result.expansion_name;
+        await this.dbConn.manager.save(professionPricelist);
+
+        return {
+            data: {
+                entries,
+                pricelist,
+                profession_pricelist: professionPricelist,
+            },
+            status: HTTPStatus.CREATED,
+        };
+    };
+
+    public deleteProfessionPricelist: RequestHandler<null, null | IValidationErrorResponse> = async req => {
+        const user = req.user!;
+        if (user.level !== UserLevel.Admin) {
+            const validationErrors: IValidationErrorResponse = { unauthorized: "You are not authorized to do that." };
+
+            return {
+                data: validationErrors,
+                status: HTTPStatus.UNAUTHORIZED,
+            };
+        }
+
+        const professionPricelist = await this.dbConn.getRepository(ProfessionPricelist).findOne({
+            where: { id: req.params["id"] },
+        });
+        if (typeof professionPricelist === "undefined") {
+            return {
+                data: null,
+                status: HTTPStatus.NOT_FOUND,
+            };
+        }
+
+        if (professionPricelist.pricelist.user.id !== user.id) {
+            return {
+                data: null,
+                status: HTTPStatus.UNAUTHORIZED,
+            };
+        }
+
+        await Promise.all(professionPricelist.pricelist.entries.map(v => this.dbConn.manager.remove(v)));
+        await this.dbConn.manager.remove(professionPricelist);
+        await this.dbConn.manager.remove(professionPricelist.pricelist);
+
+        return {
+            data: null,
+            status: HTTPStatus.OK,
+        };
+    };
+}
--- app/src/routes/user/profession-pricelists-crud.ts
+++ app/src/routes/user/profession-pricelists-crud.ts
@@ -1,79 +1,20 @@
 import { wrap } from "async-middleware";
 import { Request, Response, Router } from "express";
-import * as HTTPStatus from "http-status";
 import { Connection } from "typeorm";
 
-import { Pricelist, PricelistEntry, ProfessionPricelist, User } from "../../entities";
-import { UserLevel } from "../../entities/user";
-import { ExpansionName } from "../../lib/expansion";
-import { ProfessionName } from "../../lib/profession";
+import { handle } from "../../controllers";
+import { ProfessionPricelistsCrudController } from "../../controllers/user/profession-pricelists-crud";
 import { auth } from "../../lib/session";
-import { ProfessionPricelistRequestBodyRules } from "../../lib/validator-rules";
-
-interface IProfessionPricelistRequestBody {
-    pricelist: {
-        name: string;
-    };
-    entries: Array<{
-        id?: number;
-        item_id: number;
-        quantity_modifier: number;
-    }>;
-    profession_name: ProfessionName;
-    expansion_name: ExpansionName;
-}
 
 export const getRouter = (dbConn: Connection) => {
     const router = Router();
+    const controller = new ProfessionPricelistsCrudController(dbConn);
 
     router.post(
         "/",
         auth,
         wrap(async (req: Request, res: Response) => {
-            const user = req.user as User;
-            if (user.level !== UserLevel.Admin) {
-                res.status(HTTPStatus.UNAUTHORIZED).json({ unauthorized: "You are not authorized to do that." });
-
-                return;
-            }
-
-            let result: IProfessionPricelistRequestBody | null = null;
-            try {
-                result = (await ProfessionPricelistRequestBodyRules.validate(
-                    req.body,
-                )) as IProfessionPricelistRequestBody;
-            } catch (err) {
-                res.status(HTTPStatus.BAD_REQUEST).json({ [err.path]: err.message });
-
-                return;
-            }
-
-            const pricelist = new Pricelist();
-            pricelist.user = user;
-            pricelist.name = result.pricelist.name;
-            await dbConn.manager.save(pricelist);
-
-            const entries = await Promise.all(
-                result.entries.map(v => {
-                    const entry = new PricelistEntry();
-                    entry.pricelist = pricelist;
-                    entry.itemId = v.item_id;
-                    entry.quantityModifier = v.quantity_modifier;
-
-                    return dbConn.manager.save(entry);
-                }),
-            );
-            const professionPricelist = new ProfessionPricelist();
-            professionPricelist.pricelist = pricelist;
-            professionPricelist.name = result.profession_name;
-            professionPricelist.expansion = result.expansion_name;
-            await dbConn.manager.save(professionPricelist);
-
-            res.status(HTTPStatus.CREATED).json({
-                entries,
-                pricelist,
-                profession_pricelist: professionPricelist,
-            });
+            await handle(controller.createProfessionPricelist, req, res);
         }),
     );
 
@@ -81,32 +22,7 @@ export const getRouter = (dbConn: Connection) => {
         "/:id",
         auth,
         wrap(async (req: Request, res: Response) => {
-            const user = req.user as User;
-            if (user.level !== UserLevel.Admin) {
-                res.status(HTTPStatus.UNAUTHORIZED).json({ unauthorized: "You are not authorized to do that." });
-
-                return;
-            }
-
-            const professionPricelist = await dbConn.getRepository(ProfessionPricelist).findOne({
-                where: { id: req.params["id"] },
-            });
-            if (typeof professionPricelist === "undefined") {
-                res.status(HTTPStatus.NOT_FOUND).json({});
-
-                return;
-            }
-
-            if (professionPricelist.pricelist.user.id !== user.id) {
-                res.status(HTTPStatus.UNAUTHORIZED).json({});
-
-                return;
-            }
-
-            await Promise.all(professionPricelist.pricelist.entries.map(v => dbConn.manager.remove(v)));
-            await dbConn.manager.remove(professionPricelist);
-            await dbConn.manager.remove(professionPricelist.pricelist);
-            res.json({});
+            await handle(controller.deleteProfessionPricelist, req, res);
         }),
     );
 
