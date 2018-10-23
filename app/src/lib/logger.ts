788f121cfad6d4b179ab7955b0a76ad6f00e1bbd
Adrian Parker <ihsw.aparker@gmail.com>
2018-10-23 14:04:45 -0400

    Lower log level.

--- app/src/lib/logger.ts
+++ app/src/lib/logger.ts
@@ -1,6 +1,6 @@
 import { createLogger, format, Logger, transports } from "winston";
 
-export const getLogger = (level: string = "info"): Logger => {
+export const getLogger = (level: string = "warn"): Logger => {
     return createLogger({
         format: format.json(),
         level,
