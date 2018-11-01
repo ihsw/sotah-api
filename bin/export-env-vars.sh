b3fd4208b48701b190a0937e9f15e5766afc722e
Adrian Parker <ihsw.aparker@gmail.com>
2018-11-01 12:28:04 -0400

    Add missing env var export.

--- bin/export-env-vars.sh
+++ bin/export-env-vars.sh
@@ -4,3 +4,4 @@ export DB_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddre
 export NATS_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_nats_1`
 export NATS_PORT=4222
 export APP_PORT=8080
+export TYPEORM_HOST=$DB_HOST
