ed7f24f844f4037726440bbe0d5d0ff072ad40aa
Adrian Parker <ihsw.aparker@gmail.com>
2018-11-02 13:13:37 -0400

    Remove unnecessary check.

--- app/src/controllers/user/post-crud.ts
+++ app/src/controllers/user/post-crud.ts
@@ -3,13 +3,19 @@ import * as HTTPStatus from "http-status";
 import { Post } from "../../entities/post";
 import { IValidationErrorResponse } from "../../types/contracts";
 import { ICreatePostRequest, ICreatePostResponse } from "../../types/contracts/user/post-crud";
+import { UserLevel } from "../../types/entities";
 import { RequestHandler } from "../index";
 
 export class PostCrudController {
     public createPost: RequestHandler<
         ICreatePostRequest,
-        ICreatePostResponse | IValidationErrorResponse
-    > = async () => {
+        ICreatePostResponse | IValidationErrorResponse | null
+    > = async req => {
+        const user = req.user!;
+        if (user.level < UserLevel.Admin) {
+            return { data: null, status: HTTPStatus.UNAUTHORIZED };
+        }
+
         const post = new Post();
         post.id = -1;
 
