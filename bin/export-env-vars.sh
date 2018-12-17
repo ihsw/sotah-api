14a41ea272b33ea630d48907346c8de988c0d238
Adrian Parker <ihsw.aparker@gmail.com>
2018-12-16 20:42:14 -0500

    Update for local dev environment to prevent port conflict.

--- bin/export-env-vars.sh
+++ bin/export-env-vars.sh
@@ -3,5 +3,5 @@
 export DB_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_db_1`
 export NATS_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sotahinfra_nats_1`
 export NATS_PORT=4222
-export APP_PORT=8080
+export APP_PORT=9999
 export TYPEORM_HOST=$DB_HOST
