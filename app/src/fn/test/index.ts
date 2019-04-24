0994c2f31983611d2d8c147f324918099f4314da
Adrian Parker <ihsw.aparker@gmail.com>
2019-04-24 12:25:49 -0400

    Start api fn.

---
+++ app/src/fn/test/index.ts
@@ -0,0 +1,6 @@
+import { IPubsubMessage } from "..";
+
+export const test = (_data: IPubsubMessage, _context: unknown) => {
+    // tslint:disable-next-line:no-console
+    console.log("Hello, world!");
+};
